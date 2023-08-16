83f5cbd0131485f5039ba70f992517f07cc18bcd
ChangeDL <douglas.loizzo@gmail.com>
2023-08-16 17:11:29 -0400

    Setup a JFrame to have a main window for the application with the title "Java Swing Practice"

--- Practice.java
+++ Practice.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 
 public class Practice {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java Swing Practice");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
 }
