69df178273fec2b936d0bf31169faeca0f9c5544
ChangeDL <douglas.loizzo@gmail.com>
2023-08-17 14:09:52 -0400

    Added ActionEvent and ActionListener to trigger button, and  another label to trigger when button is clicked

--- Practice.java
+++ Practice.java
@@ -1,14 +1,32 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Practice {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Java Swing Practice");
         frame.setSize(400, 300);
         JLabel label = new JLabel("Hello, Swing!");
+        JLabel labelClick = new JLabel("");
         JButton button = new JButton("Click Me If You Dare");
 
+        frame.setLayout(new FlowLayout());
         frame.add(label);
         frame.add(button);
+        frame.add(labelClick);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String labelText = labelClick.getText();
+                int textLength = labelText.length();
+                if (textLength == 0) {
+                    labelClick.setText("WHY DID YOU CLICK ME! CLICK IT AGAIN AND HIDE ME!");
+                } else {
+                    labelClick.setText("");
+                }
+            }
+        });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
