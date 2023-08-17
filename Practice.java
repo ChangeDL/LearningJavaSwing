a382e058212ca28d07f925a00d35a4dd2d52387e
ChangeDL <douglas.loizzo@gmail.com>
2023-08-17 13:46:55 -0400

    Added a label and button

--- Practice.java
+++ Practice.java
@@ -4,6 +4,12 @@ public class Practice {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Java Swing Practice");
         frame.setSize(400, 300);
+        JLabel label = new JLabel("Hello, Swing!");
+        JButton button = new JButton("Click Me If You Dare");
+
+        frame.add(label);
+        frame.add(button);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
